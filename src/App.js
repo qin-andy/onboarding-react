622c52c4a08a0f5212889b6a2c3a26781ae2defe
Andy <andyqin17@gmail.com>
2022-04-04 15:11:50 -0700

    Fix styles and add donut

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import "./App.css";
+
+// Image attribution: https://pixabay.com/illustrations/cartoon-donuts-donut-chocolate-4764725/
 
 function App() {
   return (
-    <div id='main'>
-      <div className='column'>
-        <header>
-          Hello, Onboarding!
-        </header>
+    <div id="main">
+      <div className="column">
+        <h1>Hello, Onboarding!</h1>
+        {/* Here's where all the magic happens! */}
+        <ClickableDonut
       </div>
     </div>
   );
---
+++ src/DonutClicker/DonutButton.jsx
@@ -0,0 +1,9 @@
+function ClickableDonut(props) {
+  return (
+    <>
+      <h1>I'M A DONUT!</h1>
+    </>
+  );
+}
+
+export default ClickableDonut;
