82da65d33ce293c946821e2af2e7f2a144122f1f
Andy <andyqin17@gmail.com>
2022-04-04 14:55:23 -0700

    Delete boilerplate, setup skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id='main'>
+      <div className='column'>
+        <header>
+          Hello, Onboarding!
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
