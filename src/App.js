809befc1e303264c71b66c9918e6db170ade6f37
Andy <andyqin17@gmail.com>
2022-04-04 15:30:48 -0700

    Add SpyNameGenerator skeleton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import SpyNameGenerator from "./SpyNameGenerator/SpyNameGenerator";
 
 // Image attribution: https://pixabay.com/illustrations/cartoon-donuts-donut-chocolate-4764725/
 
@@ -8,7 +9,7 @@ function App() {
       <div className="column">
         <h1>Hello, Onboarding!</h1>
         {/* Here's where all the magic happens! */}
-        <ClickableDonut
+        <SpyNameGenerator />
       </div>
     </div>
   );
---
+++ src/SpyNameGenerator/SpyNameGenerator.jsx
@@ -0,0 +1,13 @@
+function SpyNameGenerator(props) {
+  return (
+    <>
+      <h1>Spy Name Generator</h1>
+      <h2>Previous Spy Name: {/* Something goes here! */}</h2>
+      <h2>Current Spy Name: {/* Something goes here! */}</h2>
+      <input /> {/* Something goes here! */}
+      <button>Save</button>
+    </>
+  );
+}
+
+export default SpyNameGenerator;
