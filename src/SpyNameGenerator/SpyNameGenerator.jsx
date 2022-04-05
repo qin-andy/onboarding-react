e0d7a9470a70f0a609b3350af147827954532763
Andy <andyqin17@gmail.com>
2022-04-04 17:33:19 -0700

    Update styles

--- src/SpyNameGenerator/SpyNameGenerator.jsx
+++ src/SpyNameGenerator/SpyNameGenerator.jsx
@@ -2,10 +2,18 @@ function SpyNameGenerator(props) {
   return (
     <>
       <h1>Spy Name Generator</h1>
-      <h2>Previous Spy Name: {/* Something goes here! */}</h2>
-      <h2>Current Spy Name: {/* Something goes here! */}</h2>
-      <input /> {/* Something goes here! */}
-      <button>Save</button>
+      <h2>Previous Spy Name: </h2>
+      <p>{/* Something goes here! */}</p>
+
+      <h2>Current Spy Name: </h2>
+      <p>{/* Something goes here! */}</p>
+
+      <div>
+        <input type="text"/>
+        <button className="button1">
+          Save
+        </button>
+      </div>
     </>
   );
 }
