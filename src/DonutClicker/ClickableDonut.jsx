fd5bcdf5bc4cbcf1a9f46a6a1b0f5628456c3a30
mpark63 <minspk13@gmail.com>
2022-04-17 15:51:39 -0400

    added a donut

--- src/DonutClicker/ClickableDonut.jsx
+++ src/DonutClicker/ClickableDonut.jsx
@@ -1,9 +1,44 @@
+import React, { useState } from 'react';
+
 function ClickableDonut(props) {
   return (
     <>
-      <h1>I'M A DONUT!</h1>
+      <CounterDisplay />
     </>
   );
 }
+
+const CounterDisplay = (props) => {
+  const [count, setCount] = useState(0); 
+
+  const DonutButton = (props) => {
+    const [size, setSize] = useState(100);
+    return (
+      <img src="https://cdn.pixabay.com/photo/2020/01/14/10/55/cartoon-4764725_1280.png" height={size} alt="donut.png" onClick={() => setCount(count + 1)} onMouseDown={() => setSize(size - 10)} onMouseUp={() => setSize(size + 10)}/>
+    );
+  }
+  
+  const EvilDonutButton = (props) => {
+    const [size, setSize] = useState(100);
+    return (
+      <img src="https://cdn.pixabay.com/photo/2020/01/14/10/55/cartoon-4764725_1280.png" height={size} alt="donut.png" onClick={() => setCount(count - 1)} onMouseDown={() => setSize(size - 10)} onMouseUp={() => setSize(size + 10)}/>
+    );
+  }
+
+  return (
+      <>
+      <h1>I'M A DONUT!</h1>
+      <h3>Donut count: {count}</h3>
+      <div>
+        <column><DonutButton /></column>
+        <column><EvilDonutButton /></column>
+      </div>
+      {count >= 10 && 
+        <h1>You Win! ^.^</h1>
+      }
+      </>
+  ); 
+}
+
 
 export default ClickableDonut;
--- src/SpyNameGenerator/SpyNameGenerator.jsx
+++ src/SpyNameGenerator/SpyNameGenerator.jsx
@@ -1,21 +1,56 @@
-function SpyNameGenerator(props) {
-  return (
-    <>
-      <h1>Spy Name Generator</h1>
-      <h2>Previous Spy Name: </h2>
-      <p>{/* Something goes here! */}</p>
+import React, { useState } from 'react';
 
-      <h2>Current Spy Name: </h2>
-      <p>{/* Something goes here! */}</p>
+class SpyNameGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {input: '', revInput: ''};
+    this.handleInputChange = this.handleInputChange.bind(this); 
+  }
 
-      <div>
-        <input type="text"/>
-        <button className="button1">
-          Save
-        </button>
-      </div>
-    </>
-  );
+  handleInputChange(event) {
+    this.setState({input: event.target.value}); 
+    this.setState({revInput: event.target.value.split("").reverse().join("")})
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Spy Name Generator</h1>
+        <h2>Previous Spy Name: </h2>
+        <p>{this.state.revInput}</p>
+
+        <h2>Current Spy Name: </h2>
+        <p>{this.state.input}</p>
+
+        <div>
+          <input type="text" value={this.state.input} onChange={this.handleInputChange} />
+          <button className="button1">
+            Save
+          </button>
+        </div>
+      </>
+    );
+  }
 }
 
+// function SpyNameGenerator(props) {
+//   const [input, setInput] = useState('hello'); 
+//   return (
+//     <>
+//       <h1>Spy Name Generator</h1>
+//       <h2>Previous Spy Name: </h2>
+//       <p>{/* Something goes here! */}</p>
+
+//       <h2>Current Spy Name: </h2>
+//       <p>{}</p>
+
+//       <div>
+//         <input type="text" value={input} onChange={setInput}/>
+//         <button className="button1">
+//           Save
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
 export default SpyNameGenerator;
